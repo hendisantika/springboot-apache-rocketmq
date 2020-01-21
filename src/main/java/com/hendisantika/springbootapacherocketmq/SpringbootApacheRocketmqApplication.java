6298b989acee82540229eaee89a64f921359b46b
Hendi Santika <hendisantika@gmail.com>
2020-01-21 07:30:59 +0700

    #5 - Modify Main App

--- src/main/java/com/hendisantika/springbootapacherocketmq/SpringbootApacheRocketmqApplication.java
+++ src/main/java/com/hendisantika/springbootapacherocketmq/SpringbootApacheRocketmqApplication.java
@@ -1,13 +1,26 @@
 package com.hendisantika.springbootapacherocketmq;
 
+import com.hendisantika.springbootapacherocketmq.event.CartItemEvent;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApacheRocketmqApplication {
+public class SpringbootApacheRocketmqApplication implements CommandLineRunner {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApacheRocketmqApplication.class, args);
+    }
+
+    public void run(String... args) throws Exception {
+        rocketMQTemplate.convertAndSend("cart-item-add-topic", new CartItemEvent("bike", 1));
+        rocketMQTemplate.convertAndSend("cart-item-add-topic", new CartItemEvent("computer", 2));
+        rocketMQTemplate.convertAndSend("cart-item-removed-topic", new CartItemEvent("bike", 1));
     }
 
 }
