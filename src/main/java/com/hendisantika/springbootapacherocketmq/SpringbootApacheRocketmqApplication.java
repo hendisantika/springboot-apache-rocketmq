4a40a83a5fe24a1994c43c4c036efbbb5175ed15
Hendi Santika <hendisantika@gmail.com>
2020-01-21 07:27:11 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootapacherocketmq/SpringbootApacheRocketmqApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootapacherocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApacheRocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApacheRocketmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootapacherocketmq/SpringbootApacheRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootapacherocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApacheRocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
